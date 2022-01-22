4396e4b178ec8091761645d763c5abef197aebc4
Jesus Alberto Ruiz <jaruizf@falabella.cl>
2022-01-21 22:57:00 -0300

    Setup groovy scripts

--- maven.groovy
+++ maven.groovy
@@ -15,7 +15,7 @@ def call() {
     }
   }
   stage('UploadNexus') {
-    nexusPublisher nexusInstanceId: 'nexus-local', nexusRepositoryId: 'test-repo', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: '', filePath: '/Users/jaruizf/repos/ejemplo-gradle/build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+    nexusPublisher nexusInstanceId: 'nexus-local', nexusRepositoryId: 'test-repo', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: '', filePath: '/Users/jaruizf/.jenkins/workspace/ejemplo-gradle_maven-gradle/build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
   }
 }
 
